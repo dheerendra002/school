396f3b46b16f279d157ceaf252a65a51bc30bd4c
root <micscov2@gmail.com>
2016-07-23 09:23:50 +0530

    Basic UI Infrastructure

--- vidyalaya/urls.py
+++ vidyalaya/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 #import vidyalaya.urls
+from django.conf.urls import url
+from . import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'school.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('vidyalaya.urls'))
+
+    url(r'^home$', views.index, name='index'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
+
--- vidyalaya/views.py
+++ vidyalaya/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template("home.html")
+    return HttpResponse(template.render())
