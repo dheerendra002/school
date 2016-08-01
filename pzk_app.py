c682c7ee474e681dda2c4a7070d566189929cdf6
root <micscov2@gmail.com>
2016-08-01 23:53:45 +0530

    Modified pzk_app.py

--- pzk_app.py
+++ pzk_app.py
@@ -1,12 +1,12 @@
 import smtplib
 import getpass
 
-username = raw_input("Username: ")
+send_to = raw_input("Recipient mail: ")
+msg = raw_input("Message to send: ")
+username = raw_input("Email: ")
 passwd = getpass.getpass("Password: ")
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login(username, passwd)
-msg = raw_input("Message to send: ")
-send_to = raw_input("Recipient mail: ")
 server.sendmail(username, send_to, msg)
 server.quit()
