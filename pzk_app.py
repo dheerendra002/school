b70c98998b17823b598c0023b65f563186e19e95
root <micscov2@gmail.com>
2016-08-07 14:41:00 +0530

    selenium integrated

--- pzk_app.py
+++ pzk_app.py
@@ -1,9 +1,10 @@
 import smtplib
 import getpass
+from scrape import scrape
 
+msg = scrape.get_data_status()
 send_to = raw_input("Recipient mail: ")
-msg = raw_input("Message to send: ")
-username = raw_input("Email: ")
+username = "micscov2@gmail.com"
 passwd = getpass.getpass("Password: ")
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
